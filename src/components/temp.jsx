b18f207ab3ab77617f4ae6330d9e13bbe007be43
Scorpi <scorpifr@gmail.com>
2025-07-10 23:20:22 +0200

    Rename useSession.jsx to temp.jsx

---
+++ src/components/temp.jsx
@@ -0,0 +1,42 @@
+import { useReducer, useEffect } from "react";
+
+export default function UseSession() {
+  function reducer(currState, action) {
+    switch (action.type) {
+      case "logout":
+        return {};
+      case "reset":
+        return {};
+      case "set":
+        return action.payload;
+      default:
+        return currState;
+    }
+  }
+
+  const [session, dispatchSession] = useReducer(
+    reducer,
+    null,
+    getFromLocalStorage
+  );
+
+  // init
+  function getFromLocalStorage() {
+    const storageValue = localStorage.getItem("session");
+    const res =
+      typeof storageValue !== "undefined" && storageValue !== "undefined"
+        ? JSON.parse(storageValue)
+        : null;
+    return res;
+  }
+
+  // update
+  useEffect(
+    function () {
+      localStorage.setItem("session", JSON.stringify(session));
+    },
+    [session]
+  );
+
+  return { session, dispatchSession };
+}
